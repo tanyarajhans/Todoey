6f4cca7727ea727fbf1b22c98bb40f9f64fd853e
Tanya <tanyarajhans7@gmail.com>
2022-01-12 12:55:44 +0530

    Deleting tasks & Todoey v1.0 done

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -4,12 +4,14 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
   final void Function(bool?) checkboxCallback;
+  final void Function() longPressCallback;
  
-TaskTile({required this.isChecked, required this.taskTitle, required this.checkboxCallback});
+TaskTile({required this.isChecked, required this.taskTitle, required this.checkboxCallback, required this.longPressCallback});
  
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: longPressCallback,
       title: Text(
         taskTitle,
         style: TextStyle(
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -18,6 +18,9 @@ class TasksList extends StatelessWidget {
           checkboxCallback: (checkboxState) {
             taskData.updateTask(task);
           },
+          longPressCallback: (){
+            taskData.deleteTask(task);
+          },
         );
       },
       itemCount: taskData.taskCount,
